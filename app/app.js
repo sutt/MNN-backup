f9c85b9d3234f608b73ce58f5b4367abb95a99c4
Will Sutton <wsutton17@gmail.com>
2021-02-06 14:21:34 -0500

    query for senate_districts ported

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const key = process.env.MAPS_KEY || apiKey[0]['key']
 const app = express()
 
 app.use('/js', express.static('./app/js'));
+app.use('/data', express.static('./app/data'));
 app.set('views', './app/views/')
 app.set('view engine', 'hbs')
 
